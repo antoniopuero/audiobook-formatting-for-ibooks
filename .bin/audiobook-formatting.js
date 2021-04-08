b11ab749878da48483cf14cb31eec0b9f066eb24
Anton Savchenko <antonio.puero@gmail.com>
2021-04-08 20:29:57 +0300

    initial version of audiobook formatter

---
+++ .bin/audiobook-formatting.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const { program } = require('commander');
+const ffmetadata = require('ffmetadata');
+
+program.version('0.0.1');
+
+program
+  .requiredOption('-i, --input <type>', 'relative path to audiobook root folder')
+  .option('-a, --album <type>', 'album name, might be needed if audiobook is still imported as separate files');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+async function* getFiles(dir) {
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = path.resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else if (!/ds_store/i.test(dirent.name)) {
+      yield res;
+    }
+  }
+}
+
+console.log(options)
+
+const directoryPath = path.join(process.cwd(), options.input);
+
+
+const writeMeta = promisify(ffmetadata.write);
+const readMeta = promisify(ffmetadata.read);
+
+async function updateMeta() {
+    const coverPath = `/tmp/${Math.random().toString(36).substr(2, 9)}.png`;
+
+    let coverExists = false;
+    let i = 1;
+    for await (let file of getFiles(directoryPath)) {
+      if (i === 1) {
+        // only to save cover
+        await readMeta(file, { coverPath });
+        coverExists = fs.existsSync(coverPath);
+      }
+
+      await writeMeta(file, {
+        track: i,
+          ...(options.album ? { album: options.album } : {}),
+        }, (coverExists ? {
+          attachments: [coverPath],
+        } : {}));
+      i++;
+    }
+ 
+    await fs.promises.unlink(coverPath);
+}
+
+updateMeta();
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # audiobook-formatting-for-ibooks
-Node script which takes the audiobook files and applies track number &amp; album name so the files are groupped correctly by iBooks
+Node cli tool, which takes the audiobook files and applies track number &amp; album name so the files are groupped correctly by iBooks
+
+# Important
+Don't run the script immediately on your only copy of files! This script is changing files in place, so be sure to test it out on smaller bunch first - you would want to copy those into a separate folder and try it out.
+
+# Usage
+## Dependencies
+node v^14
+ffmetadata module relies on http://www.ffmpeg.org/ so be sure to install it first
+
+## How to run
+1. `npm i -g audiobook-formatting`
+2.  run `audiobook-formatting -i <path>`, where path is relative path from current folder to the audiobook root folder
+3. if after step above the audiobook is still imported into iBooks as a bucnch of different audiobooks - try to use `-a <custom_album_name>` option. This will rename the album in each media file and after that iBooks will treat them as 1 audiobook.
+
